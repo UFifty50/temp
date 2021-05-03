48aed7747bd40cf63cb7a64d9186e74ed2b7316c
UFifty50 <64788389+UFifty50@users.noreply.github.com>
2021-05-03 09:25:43 +0100

    Update prep_mc.bat

--- prep_mc.bat
+++ prep_mc.bat
@@ -1,4 +1,8 @@
 powershell -Command "Stop-Process -Name "ProcessName" -Force"
+echo [92mDownloading fabric jar[0m
+powershell -Command "Invoke-WebRequest https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.7.3/fabric-installer-0.7.3.jar -OutFile %TMP%\fabric.jar || echo [42mDownload Failed, please check your internet connection[0m && exit 1"
+echo [92mInstalling fabric[0m
+java -jar %TMP%\fabric.jar || echo [42mUnzip failed, do you have the correct permissions?[0m && exit 1
 echo [92mDownloading zip of the required mods[0m
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/UFifty50/temp/main/mods.zip -OutFile %TMP%\mods.zip || echo [42mDownload Failed, please check your internet connection[0m && exit 1"
 echo [92mUnzipping mods zip[0m
